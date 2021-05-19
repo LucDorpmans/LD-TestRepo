a68db511917536876903762e8d2ba1fcf61d9b22
LucDorpmans <GitHub@sure.nl>
2021-05-19 19:06:07 +0200

    My changes

--- GetMyScripts.ps1
+++ GetMyScripts.ps1
@@ -1,10 +1,10 @@
 #GetMyScripts.ps1
 $MyFile = "EdgeMSI-DownloadComplete.ps1"
-Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$MyFile"  -Outfile "C:\Temp\$MyFile" 
+Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$MyFile"  -Outfile "$env:USERPROFILE\Downloads\$MyFile" 
 
 $MyFile = "Chrome-Download+RunInstaller.ps1"
-Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$MyFile"  -Outfile "C:\Temp\$MyFile"
+Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$MyFile"  -Outfile "$env:USERPROFILE\Downloads\$MyFile"
 
 $MyFile = "WAC-Download+Install.ps1"
-Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$MyFile"  -Outfile "C:\Temp\$MyFile"
+Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$MyFile"  -Outfile "$env:USERPROFILE\Downloads\$MyFile"
 
