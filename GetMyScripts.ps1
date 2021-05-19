ead8a4da57f2a01d91d3b4de0928946c712f6b77
LucDorpmans <GitHub@sure.nl>
2021-05-19 19:10:24 +0200

    Update GetMyScripts.ps1

--- GetMyScripts.ps1
+++ GetMyScripts.ps1
@@ -2,7 +2,7 @@
 $MyFile = "EdgeMSI-DownloadComplete.ps1"
 Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$MyFile"  -Outfile "$env:USERPROFILE\Downloads\$MyFile" 
 
-$MyFile = "Chrome-Download+RunInstaller.ps1"
+$MyFile = "Chrome-Download+Run-Installer.ps1"
 Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$MyFile"  -Outfile "$env:USERPROFILE\Downloads\$MyFile"
 
 $MyFile = "WAC-Download+Install.ps1"
