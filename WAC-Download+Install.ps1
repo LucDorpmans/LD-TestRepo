1ee84485d5a17ef041f87f291dcd99efac501ae3
LucDorpmans <GitHub@sure.nl>
2021-05-19 18:13:17 +0200

    Add files via upload

---
+++ WAC-Download+Install.ps1
@@ -0,0 +1,5 @@
+﻿$progressPreference='SilentlyContinue'
+
+Invoke-WebRequest -UseBasicParsing -Uri https://aka.ms/WACDownload -OutFile "$env:USERPROFILE\Downloads\WindowsAdminCenter.msi"
+Start-Process msiexec.exe -Wait -ArgumentList "/i $env:USERPROFILE\Downloads\WindowsAdminCenter.msi /qn /L*v waclog.txt REGISTRY_REDIRECT_PORT_80=1 SME_PORT=443 SSL_CERTIFICATE_OPTION=generate"
+
