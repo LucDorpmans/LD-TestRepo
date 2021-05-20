52967e5506a84914c4d25b11af37ab35905aeea0
LucDorpmans <GitHub@sure.nl>
2021-05-20 13:49:59 +0200

    Add files via upload

--- GetMyScriptF.ps1
+++ GetMyScriptF.ps1
@@ -1,28 +1,12 @@
 #GetMyScriptF.ps1
 Function Get-MyScript 
-{
-    [CmdletBinding()]
-    Param
-    (
-       # Filename of script to download
-        [Parameter(Mandatory=$true,
-                   Position=0)]
+{    [CmdletBinding()]
+    Param    ( 
+        [Parameter(Mandatory=$true,Position=0)]
         [string]$AFile,
-        # Location to save file, default is in the download folder of current user
-        [string]
-        $SaveLocation = "$env:USERPROFILE\Downloads\"
-    )
-
-    Process
-    {
-        Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$AFile"  -Outfile "$SaveLocation$AFile"
-    }
-}
-
+        [string]$SaveLocation = "$env:USERPROFILE\Downloads\"     )
+        Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$AFile"  -Outfile "$SaveLocation$AFile" }
 
 Get-MyScript "EdgeMSI-DownloadComplete.ps1"
-
 Get-MyScript "Chrome-Download+Run-Installer.ps1"
-
-Get-MyScript  "WAC-Download+Install.ps1"
-
+Get-MyScript "WAC-Download+Install.ps1"
