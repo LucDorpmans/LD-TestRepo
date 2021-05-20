e4f3f6c81eca0564e48539ec7cbd4502923f8669
LucDorpmans <GitHub@sure.nl>
2021-05-20 13:29:57 +0200

    Update GetMyScriptF.ps1

--- GetMyScriptF.ps1
+++ GetMyScriptF.ps1
@@ -1,39 +1,21 @@
 #GetMyScriptF.ps1
-
-<#
-.Synopsis
-   Get A Script from My GitHub Repository
-.DESCRIPTION
-   Get A Script from My GitHub Repository
-.EXAMPLE
-   Get-MyScript
-.EXAMPLE
-   Another example of how to use this cmdlet
-#>
 Function Get-MyScript 
 {
     [CmdletBinding()]
     Param
     (
-        # Filename of script to download
+       # Filename of script to download
         [Parameter(Mandatory=$true,
                    Position=0)]
         [string]$AFile,
-
         # Location to save file, default is in the download folder of current user
         [string]
         $SaveLocation = "$env:USERPROFILE\Downloads\"
     )
 
-    Begin
-    {
-    }
     Process
     {
         Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$AFile"  -Outfile "$SaveLocation$AFile"
-    }
-    End
-    {
     }
 }
 
