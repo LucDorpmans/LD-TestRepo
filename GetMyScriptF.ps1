2f71acaf49f004933b9402d3abc9a63eea9d8107
LucDorpmans <GitHub@sure.nl>
2021-05-25 01:34:20 +0200

    Add files via upload

--- GetMyScriptF.ps1
+++ GetMyScriptF.ps1
@@ -1,22 +1,14 @@
 #GetMyScriptF.ps1
-Function Get-MyScript ()
-{ Param    ( 
+Function Get-MyScript 
+{    [CmdletBinding()]
+    Param    ( 
         [Parameter(Mandatory=$true,Position=0)]
         [string]$AFile,
         [string]$SaveLocation = "$env:USERPROFILE\Downloads\"     )
-        Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$AFile"  -Outfile "$SaveLocation$AFile"
-        PSEdit "$SaveLocation$AFile"}
+        Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$AFile"  -Outfile "$SaveLocation$AFile" }
 
-Get-MyScript "EdgeMSI-DownloadComplete.ps1"
 Get-MyScript "Chrome-Download+Run-Installer.ps1"
 Get-MyScript "WAC-Download+Install.ps1"
+Get-MyScript "EdgeMSI-Download-Only-Complete.ps1"
+Get-MyScript "Edge-InstallOnly.ps1"
 
-<# Module 4:
-Get-MyScript Mod4A-LabVM-Instructions.ps1
-Get-MyScript Mod4A-LabVM-LabConfig.ps1
-Get-MyScript Mod4A-LabVM-Scenario_Part1.ps1
-
-Get-MyScript Mod4A-Mgmt-MultiNodeConfig.psd1
-Get-MyScript Mod4A-Mgmt-SDNExpress.ps1
-Get-MyScript Mod4A-Mgmt-Scenario_Part2.ps1
-#>
