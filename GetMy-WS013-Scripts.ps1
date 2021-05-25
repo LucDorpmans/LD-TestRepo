0d115bf5f42d8d6e17d19f0ca4ed2f8476e0494d
LucDorpmans <GitHub@sure.nl>
2021-05-25 02:10:25 +0200

    Update GetMy-WS013-Scripts.ps1

--- GetMy-WS013-Scripts.ps1
+++ GetMy-WS013-Scripts.ps1
@@ -5,7 +5,8 @@ Function Get-MyScript
         [Parameter(Mandatory=$true,Position=0)]
         [string]$AFile,
         [string]$SaveLocation = "$env:USERPROFILE\Downloads\"     )
-        Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-013-Repo.git/main/$AFile"  -Outfile "$SaveLocation$AFile" }
+        Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-013-Repo.git/main/$AFile"  -Outfile "$SaveLocation$AFile" 
+        PSEdit  ("$SaveLocation$AFile" }
 
 Get-MyScript "Chrome-Download+Run-Installer.ps1"
 Get-MyScript "WAC-Download+Install.ps1"
