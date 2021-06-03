d501a7b37db0a1f567a4e05635ffbeec46eb9386
LucDorpmans <GitHub@sure.nl>
2021-06-04 00:36:12 +0200

    Create GetMyScripts-Long.ps1

---
+++ GetMyScripts-Long.ps1
@@ -0,0 +1,16 @@
+# GetMy-WS013-Scripts-Long.ps1
+Function Get-MyScript 
+{    [CmdletBinding()]
+    Param    ( 
+        [Parameter(Mandatory=$true,Position=0)]
+        [string]$AFile,
+        [string]$SaveLocation = "$env:USERPROFILE\Downloads\", 
+		[switch]$EditFile = $False )
+        Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$AFile"  -Outfile "$SaveLocation$AFile" 
+		If ($EditFile) { PSEdit  ("$SaveLocation$AFile" )} }
+
+Get-MyScript "EdgeMSI-DownloadComplete.ps1"
+Get-MyScript "Edge-InstallOnly.ps1"
+Get-MyScript "Download+Install+PowerShell-Core.ps1" -EditFile
+Get-MyScript "WAC-Download+Install.ps1"
+Get-MyScript "Chrome-Download+Run-Installer.ps1"
