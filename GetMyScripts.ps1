24281ea9c2ce47a222406e2656d34f32882af53e
LucDorpmans <GitHub@sure.nl>
2021-06-04 00:34:05 +0200

    Add latest GetMyScripts

---
+++ GetMyScripts.ps1
@@ -0,0 +1,10 @@
+# GetMy-Scripts.ps1
+Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False, 
+							   [string]$SPath = "$env:USERPROFILE\Downloads\")
+			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$AFile"  -Outfile "$SPath$AFile" }
+
+Get-MyScript "EdgeMSI-DownloadComplete.ps1"
+Get-MyScript "Edge-InstallOnly.ps1"
+Get-MyScript "Download+Install+PowerShell-Core.ps1" -EditFile
+Get-MyScript "WAC-Download+Install.ps1"
+Get-MyScript "Chrome-Download+Run-Installer.ps1"
