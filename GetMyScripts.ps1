07dd220010226ef80cdeb5c613f1e85168c812e1
LucDorpmans <GitHub@sure.nl>
2021-06-04 00:49:54 +0200

    Update GetMyScripts.ps1

--- GetMyScripts.ps1
+++ GetMyScripts.ps1
@@ -1,9 +1,10 @@
 # GetMyScripts.ps1
 Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False, 
 							   [string]$SPath = "$env:USERPROFILE\Downloads\")
-			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$AFile"  -Outfile "$SPath$AFile" }
-
-Get-MyScript "EdgeMSI-Download-Only-Complete.ps1"
+			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/TestRepo/main/$AFile"  -Outfile "$SPath$AFile" 
+			If ($EditFile) { PSEdit  ("$SaveLocation$AFile" )} }
+		
+Get-MyScript "EdgeMSI-DownloadComplete.ps1"
 Get-MyScript "Edge-InstallOnly.ps1"
 Get-MyScript "Download+Install+PowerShell-Core.ps1" -EditFile
 Get-MyScript "WAC-Download+Install.ps1"
