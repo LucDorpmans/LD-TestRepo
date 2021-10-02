005fe2da1e85a73e28fa38560fad5f82a70988bd
LucDorpmans <GitHub@sure.nl>
2021-10-02 22:20:45 +0200

    Update NuGet+PSGallery-Short.ps1

--- NuGet+PSGallery-Short.ps1
+++ NuGet+PSGallery-Short.ps1
@@ -1,4 +1,4 @@
 #[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" # For older Windows versions
-Install-PackageProvider nuget -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-PackageProvider nuget -Force
 Get-PackageProvider -Name Nuget
