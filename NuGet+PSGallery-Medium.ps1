ff03009be67f93c46e24e68f8fe051c9a8a7dc74
LucDorpmans <GitHub@sure.nl>
2021-10-02 22:23:14 +0200

    Upd

--- NuGet+PSGallery-Medium.ps1
+++ NuGet+PSGallery-Medium.ps1
@@ -1,9 +1,9 @@
 #[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" # For older Windows versions
 
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
 $Res = Get-PackageProvider NuGet -EA SilentlyContinue 
 If ($Null -eq $Res) { Install-PackageProvider nuget -Force } 
-
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
 $NuProv = Get-PackageProvider -Name Nuget
 $NuProv.Version
