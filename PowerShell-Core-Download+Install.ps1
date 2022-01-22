ede7c02ba1e2219ddb25ba467fc383d1f9833da7
LucDorpmans <GitHub@sure.nl>
2022-01-22 15:37:28 +0100

    Update PowerShell-Core-Download+Install.ps1

--- PowerShell-Core-Download+Install.ps1
+++ PowerShell-Core-Download+Install.ps1
@@ -1,6 +1,6 @@
 $progressPreference='SilentlyContinue'
-$MyFile = "PowerShell-7.1.5-win-x64.msi"
-Invoke-Webrequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.1.5/PowerShell-7.1.5-win-x64.msi"  -Outfile "$env:USERPROFILE\Downloads\$MyFile"
+$MyFile = "PowerShell-7.2.1-win-x64.msi"
+Invoke-Webrequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/PowerShell-7.2.1-win-x64.msi"  -Outfile "$env:USERPROFILE\Downloads\$MyFile"
 Get-ChildItem "$env:USERPROFILE\Downloads\$MyFile"
 Write-Output "Starting installation of $MyFile"
 Start-Process msiexec.exe -Wait -ArgumentList "/i $env:USERPROFILE\Downloads\$MyFile /qn /quiet"
